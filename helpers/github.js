5eada7a6230d68a88574632e71e7efeb91cb7dc4
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-21 16:21:11 -0400

    Add ability to fetch user repos from GitHub API

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,14 @@
 const axios = require('axios');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
-  // TODO - Use the axios module to request repos for a specific
-  // user from the github API
-
-  // The options object has been provided to help you out, 
-  // but you'll have to fill in the URL
-  let options = {
-    url: 'FILL ME IN',
+let getReposByUsername = (username) => {
+  return axios({
+    url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
-    }
-  };
-
+    },
+  }).then((response) => response.data);
 }
 
 module.exports.getReposByUsername = getReposByUsername;
