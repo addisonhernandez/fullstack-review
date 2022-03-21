5c2b71414d18c71988235a3eb62a5d9d9264f1a9
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-21 16:25:53 -0400

    Route POST requests to /repos

--- server/index.js
+++ server/index.js
@@ -1,13 +1,33 @@
 const express = require('express');
+
+const { getReposByUsername } = require('../helpers/github');
+const db = require('../database');
+
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post('/repos', function (req, res) {
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+
+  const { username } = req.body;
+  console.log(`Serving POST to /repos for username ${username}`);
+
+  getReposByUsername(username)
+    .then((results) => {
+      res.status(201).send(results);
+      return results;
+    })
+    // .then(db.save(username, results))
+    .catch((err) => {
+      console.log('Something went wrong in app.post');
+      console.error(err);
+    });
 });
 
 app.get('/repos', function (req, res) {
@@ -17,7 +37,6 @@ app.get('/repos', function (req, res) {
 
 let port = 1128;
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`listening on port ${port}`);
 });
-
