f364a3fd4841deb28e312f7ff527bf8ff09a6edd
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-21 17:36:48 -0400

    Update schema. Initial layout for save() method

--- database/index.js
+++ database/index.js
@@ -2,22 +2,43 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  login: String,
-  repoName: String,
+  username: String,
+  reponame: String,
+  owner: String,
   url: String,
+  description: String,
   created: Date,
   updated: Date,
+  pushed: Date,
   stargazers: Number,
   watchers: Number,
-  forks: Number
+  forks: Number,
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
-}
+/**
+ * Saves one or more repos to the database
+ * @param {String} username
+ * @param {Object[]} repoData
+ */
+let save = (username, repoData) => {
+  const repos = repoData.map((repo) => {
+    return {
+      username: username,
+      reponame: repo.name,
+      url: repo.html_url,
+      description: repo.description,
+      created: repo.created_at,
+      updated: repo.updated_at,
+      pushed: repo.pushed_at,
+      stargazers: repo.stargazers_count,
+      watchers: repo.watchers_count,
+      forks: repo.forks,
+    };
+  });
+
+  Repo.insertMany(repos, { upsert: true });
+};
 
 module.exports.save = save;
