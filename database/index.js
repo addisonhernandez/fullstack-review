e6a9055bb0e70444b88b89068051691c7f2f1cd4
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-21 13:55:01 -0400

    Add an initial repoSchema

--- database/index.js
+++ database/index.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  login: String,
+  repoName: String,
+  url: String,
+  created: Date,
+  updated: Date,
+  stargazers: Number,
+  watchers: Number,
+  forks: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
