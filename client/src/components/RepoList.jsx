d9daddf0c74992292e4257b8e02544f482e381e6
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-22 14:27:41 -0400

    Dynamically render RepoListEntry Components
    Also make them look a bit prettier.

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import RepoListEntry from './RepoListEntry.jsx';
 
-const RepoList = (props) => (
+const RepoList = (props) => {
+  const entryDivStyle = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    alignItems: 'flex-start',
+  };
+
+  return (
   <div>
-    <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
+    <h2> Repo List </h2>
+    Here are the top {props.repos.length} repos.
+    <div style={entryDivStyle}>
+      {props.repos.map((repo) => (
+        <RepoListEntry repo={repo} key={repo._id} />
+      ))}
+    </div>
   </div>
-)
+)};
 
 export default RepoList;
