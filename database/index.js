d4f5009889cf088097043fd055448f08ee6352a1
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-22 15:10:10 -0400

    Refactor to use environment variables

--- database/index.js
+++ database/index.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect('mongodb://localhost/fetcher', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-});
+}).then(() => console.log('Connected to MongoDB'));
 
 let repoSchema = mongoose.Schema({
   username: String,
--- helpers/github.js
+++ helpers/github.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
-const config = require('../config.js');
+require('dotenv').config();
 
 let getReposByUsername = (username) => {
   return axios({
     url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
+      Authorization: process.env.GITHUB_TOKEN,
     },
   }).then((response) => response.data);
-}
+};
 
 module.exports.getReposByUsername = getReposByUsername;
